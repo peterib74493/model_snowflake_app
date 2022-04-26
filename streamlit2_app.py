08da7ea6dd54aeb8c026ed09116de6ce7b72b9bb
peterib74493 <104122221+peterib74493@users.noreply.github.com>
2022-04-27 10:11:17 +1200

    Update streamlit2_app.py

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Mom\'s new healthy diner')
-streamlit.header (:satisfied:'Breakfast Favourites')
+streamlit.header :satisfied: ('Breakfast Favourites')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach and Rocket Smoothie')
 streamlit.text ('Hard-boiled free-range egg')
